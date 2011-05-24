af86f8c12783c1c62706b340b4dbafbf55d5ffd8
saturn1.mc@gmail.com <saturn1.mc@gmail.com@fac5e847-8771-6dee-5d94-998d05010f92>
2011-05-24 16:34:06 +0000

    Separate behaviour

--- PPBot.h
+++ PPBot.h
@@ -5,19 +5,23 @@
 #include <Qt\qpainter.h>
 #include <Qt\qvector2d.h>
 
+class PPEnvironment;
+
 class PPBot{
 
 protected:
 	const double PI;
 
-	const double _radius;
-	const double _slowing_distance;
-	const double _mass;
-	const double _max_speed;
-	const double _max_force;
-	const double _prediction_coeff;
+	double _radius;
+	double _slowing_distance;
+	double _mass;
+	double _max_speed;
+	double _max_force;
+	double _prediction_coeff;
 
 	double _speed;
+
+	PPEnvironment* _environment;
 
 	QPointF* _position;
 	QPointF* _futurePosition;
@@ -32,81 +36,23 @@ protected:
 
 	QPolygonF* _sight;
 
-	virtual void updateFuturePosition() {                 
-		(*_futurePosition) = QPoint(_position->x() + (_velocity->x() * _prediction_coeff), _position->y() + (_velocity->y() * _prediction_coeff));        
-	} 
+	virtual void updateFuturePosition();
+	virtual void updateForces(void);
+	virtual void computeCorrections(void);
 
-	virtual void updateForces(void){
-		(*_steering) += truncate((*_correction), _max_force);
-		_steering->normalize();
-
-		/* Computing acceleration */
-		QVector2D steeringForce(truncate((*_steering), _max_force));
-		QVector2D acceleration(steeringForce);
-		acceleration*=(1.0 / _mass);
-
-		/* Computing velocity */
-		QVector2D vForce(_velocity->toPointF());
-		vForce += acceleration;
-
-		(*_velocity) = truncate(vForce, _speed);
-
-	}
-
-	virtual void computeCorrections(void) {
-		/* Speed corrections */
-		QPointF targetOffset = (*_target) - (*_position);
-		double distance = targetOffset.manhattanLength();
-		double rampedSpeed = _max_speed * (distance / _slowing_distance);
-
-		double clippedSpeed;
-
-		if(rampedSpeed < _max_speed){
-			clippedSpeed = rampedSpeed;
-		}
-		else{
-			clippedSpeed = _max_speed;
-		}
-
-		if(clippedSpeed < 0.01){
-			_speed = 0;
-		}
-		else{
-			_speed = clippedSpeed;
-		}
-
-		/* Trajectory corrections*/
-		(*_correction) = QVector2D(_target->x() - _futurePosition->x(), _target->y() - _futurePosition->y());
-	}
-
-	QVector2D truncate(QVector2D v, double max) {
-		QVector2D vT(v);
-		double l = vT.length();   
-		if (l > max) {     
-			vT *= (max/l);    
-		}
-		return vT;   
-	} 
-
-	void drawVector(QPainter* painter, QVector2D* v, const QColor& c, double scale) {
-		painter->setBrush(c);
-		painter->setPen(c);
-		painter->drawLine((int) _position->x(), (int) _position->y(), (int) (_position->x() + (v->x() * scale)), (int) (_position->y() + (v->y() * scale)));
-	}
-
-
-	void drawPoint(QPainter* painter, QPointF* p, const QColor& c, double radius) {
-		painter->setBrush(c);
-		painter->setPen(c);
-		painter->drawEllipse((int) (p->x() - (radius / 2.0)), (int) (p->y() - (radius / 2.0)), (int) radius, (int) radius);
-	}
+	QVector2D truncate(QVector2D v, double max);
+	void drawVector(QPainter* painter, QVector2D* v, const QColor& c, double scale);
+	void drawPoint(QPainter* painter, QPointF* p, const QColor& c, double radius);
 
 
 private:
-	PPBot(const PPBot&) : PI(atan(1.0) * 4.0), _radius(15.0), _slowing_distance(100), _mass(1.0), _max_speed(1.0), _max_force(0.05), _prediction_coeff(60.0) {}
+	PPBot(const PPBot&) : PI(atan(1.0) * 4.0), _radius(15.0), _slowing_distance(100), _mass(1.0), _max_speed(1.0), _max_force(0.05), _prediction_coeff(60.0) { _environment = 0; }
 
 public:
 	PPBot(void) : PI(atan(1.0) * 4.0), _radius(15.0), _mass(1.0), _slowing_distance(100), _max_speed(1.0), _max_force(0.05), _prediction_coeff(60.0){
+		
+		_environment = 0;
+		
 		_speed = 0;
 
 		_position = new QPointF(0, 0);
@@ -137,6 +83,10 @@ public:
 		//Nothing
 	}
 
+	void setEnvironment(PPEnvironment* environment){
+		_environment = environment;
+	}
+
 	void setTarget(const QPointF& target){
 		(*_target) = target;
 	}
@@ -149,58 +99,16 @@ public:
 		(*_position) = position;
 	}
 
-	const QPointF& getPosition(void) const{
+	const QPointF getPosition(void) const{
 		return (*_position);
 	}
 
-	virtual void draw(QPainter* painter){
-		//TEST
-		painter->setBrush(Qt::blue);
-		painter->drawEllipse((int) (_position->x() - (_radius / 2.0)), (int) (_position->y() - (_radius / 2.0)), (int) _radius, (int) _radius);
-		
-		/* Its sight rectangle*/
-		painter->setBrush(Qt::red);
-		painter->drawPolygon(*_sight);
-
-		/* Its velocity */
-		drawVector(painter, _velocity, QColor(255,0,255), 20.0);
-
-		/* Its trajectory correction */
-		drawVector(painter, _correction, QColor(0,255,0), 1.0);
-
-		/* Its local space*/
-		drawVector(painter, _forward, QColor(0,0,255), 20.0);
-		drawVector(painter, _side, QColor(0,0,255), 20.0);
-
-		/* Its future position and the corresponding point on the road */
-		drawPoint(painter, _futurePosition, QColor(255,0,0), 8.0);
-		drawPoint(painter, _target, QColor(0,0,0), 8.0);
-		//END TEST
+	const QVector2D getForward(void) const{
+		return (*_forward);
 	}
 
-	virtual void update(void){
-		/* Updating forces / trajectories */
-		updateForces();
-		updateFuturePosition();
-		computeCorrections();
-
-		/* Updating local space */
-		(*_forward) = (*_velocity);
-		_forward->normalize();
-
-		(*_side) = QVector2D((_forward->x() * cos(PI / 2.0)) - (_forward->y() * sin(PI / 2.0)), (_forward->y()  * cos(PI / 2.0)) + (_forward->x()  * sin(PI / 2.0)));
-		_side->normalize();
-		
-		/* Updating position */
-		(*_position) += (_velocity->toPointF());
-
-		/* Updating sight */
-		_sight->clear();
-		(*_sight) << (QPointF(_position->x() + (_side->x() * (_radius / 2.0)), _position->y() + (_side->y() * (_radius / 2.0))));
-		(*_sight) << (QPointF(_position->x() - (_side->x()* (_radius / 2.0)), _position->y() - (_side->y() * (_radius / 2.0))));
-		(*_sight) << (QPointF(_futurePosition->x() - (_side->x() * (_radius / 2.0)), _futurePosition->y() - (_side->y() * (_radius / 2.0))));
-		(*_sight) << (QPointF(_futurePosition->x() + (_side->x() * (_radius / 2.0)), _futurePosition->y() + (_side->y() * (_radius / 2.0))));
-	}
+	virtual void draw(QPainter* painter);
+	virtual void update(void);
 };
 
 #endif //PP_BOT_H
--- PPEnvironment.cpp
+++ PPEnvironment.cpp
@@ -1,20 +1,19 @@
 #include "PPEnvironment.h"
 
 void PPEnvironment::setMousePosition(const QPoint& pos){
-	for(QList<PPBot*>::iterator botIter = _bots.begin(); botIter != _bots.end(); botIter++){
+	for(QList<PPBot*>::iterator botIter = _bots->begin(); botIter != _bots->end(); botIter++){
 		(*botIter)->setMousePosition(pos);
 	}
 }
 
 void PPEnvironment::draw(QPainter* painter){
-
-	for(QList<PPBot*>::iterator botIter = _bots.begin(); botIter != _bots.end(); botIter++){
+	for(QList<PPBot*>::iterator botIter = _bots->begin(); botIter != _bots->end(); botIter++){
 		(*botIter)->draw(painter);
 	}
 }
 
 void PPEnvironment::update(void){
-	for(QList<PPBot*>::iterator botIter = _bots.begin(); botIter != _bots.end(); botIter++){
+	for(QList<PPBot*>::iterator botIter = _bots->begin(); botIter != _bots->end(); botIter++){
 		(*botIter)->update();
 	}
 }
